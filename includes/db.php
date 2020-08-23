64883a61c2202b7b72f9de578245d0226e2292d7
Abdul Moeez <flakerocks71@gmail.com>
2020-08-23 05:54:18 +0500

    db info updated

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,10 @@
 <?php ob_start();
 
-$db['db_host'] = "localhost";
-$db['db_user'] = "root";
-$db['db_pass'] = "root";
-$db['db_name'] = "cms";
+// ENTER DATABASE CREDENTIALS
+$db['db_host'] = "";
+$db['db_user'] = "";
+$db['db_pass'] = "";
+$db['db_name'] = "";
 
 foreach($db as $key => $value){
 define(strtoupper($key), $value);
